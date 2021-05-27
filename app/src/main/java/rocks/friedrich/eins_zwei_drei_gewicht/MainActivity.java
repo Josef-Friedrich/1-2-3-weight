cd7fce427c9b7f33ffd4009749549173ccd12d79
Josef Friedrich <josef@friedrich.rocks>
2021-05-27 20:17:32 +0200

    Add android boilerplate code

---
+++ app/src/main/java/rocks/friedrich/eins_zwei_drei_gewicht/MainActivity.java
@@ -0,0 +1,14 @@
+package rocks.friedrich.eins_zwei_drei_gewicht;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
