f29775cf8b9e73231fe19334463721f093778c49
Josef Friedrich <josef@friedrich.rocks>
2021-05-29 21:51:48 +0200

    Prototyp

--- app/src/main/java/rocks/friedrich/eins_zwei_drei_gewicht/MainActivity.java
+++ app/src/main/java/rocks/friedrich/eins_zwei_drei_gewicht/MainActivity.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
 
+public class MainActivity extends AppCompatActivity {
+    double kalorien;
     double fett;
     double portion;
-    double kalorien;
 
-    TextView ergebnisTextAnsicht;
+    TextView punkteTextAnsicht;
+    TextView punkteProPortionTextAnsicht;
 
     double berechnePunkte() {
         if (fett != 0 && kalorien != 0) {
@@ -23,15 +26,32 @@ public class MainActivity extends AppCompatActivity {
         return 0;
     }
 
-    void aktualisiereErgebnisTextAnsicht() {
-        if (ergebnisTextAnsicht != null) {
-            double punkte = berechnePunkte();
-            if (punkte > 0) {
-                ergebnisTextAnsicht.setText(Double.toString(punkte));
-            } else {
-                ergebnisTextAnsicht.setText("");
-            }
+    double berechnePunkteProPortion(double punkte) {
+        if (punkte != 0 && portion != 0) {
+            return punkte * portion / 100;
         }
+        return 0;
+    }
+
+    void aktualisiereErgebnisTextAnsicht() {
+        double punkte = berechnePunkte();
+        if (punkte > 0) {
+            punkteTextAnsicht.setText(rundePunkte(punkte));
+        } else {
+            punkteTextAnsicht.setText("Punkte");
+        }
+
+        double punkteProPortion = berechnePunkteProPortion(punkte);
+        if (punkteProPortion > 0) {
+            punkteProPortionTextAnsicht.setText(rundePunkte(punkteProPortion));
+        } else {
+            punkteProPortionTextAnsicht.setText("Punkte pro Portion");
+        }
+    }
+
+    String rundePunkte(double zahl) {
+        DecimalFormat df=new DecimalFormat("0.0");
+        return df.format(zahl);
     }
 
     double konvertiereEingabe(Editable s) {
@@ -50,7 +70,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ergebnisTextAnsicht = (TextView) findViewById(R.id.ergebnis);
+        punkteTextAnsicht = (TextView) findViewById(R.id.punkte);
+        punkteProPortionTextAnsicht = (TextView) findViewById(R.id.punkteProPortion);
+
+        EditText kalorienTextFeld = (EditText) findViewById(R.id.kalorien);
+        kalorienTextFeld.requestFocus();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        kalorienTextFeld.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                kalorien = konvertiereEingabe(s);
+                aktualisiereErgebnisTextAnsicht();
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
 
         EditText fettTextFeld = (EditText) findViewById(R.id.fett);
         fettTextFeld.addTextChangedListener(new TextWatcher() {
@@ -76,16 +111,6 @@ public class MainActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {}
         });
 
-        EditText kalorienTextFeld = (EditText) findViewById(R.id.kalorien);
-        kalorienTextFeld.addTextChangedListener(new TextWatcher() {
-            public void afterTextChanged(Editable s) {
-                kalorien = konvertiereEingabe(s);
-                aktualisiereErgebnisTextAnsicht();
-            }
 
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-        });
     }
 }
