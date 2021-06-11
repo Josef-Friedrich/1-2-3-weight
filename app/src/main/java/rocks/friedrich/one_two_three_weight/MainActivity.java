c857cb84a0390611b78e151028753254e32c8d93
Josef Friedrich <josef@friedrich.rocks>
2021-06-11 18:15:11 +0200

    Add indicator image

--- app/src/main/java/rocks/friedrich/one_two_three_weight/MainActivity.java
+++ app/src/main/java/rocks/friedrich/one_two_three_weight/MainActivity.java
@@ -1,11 +1,14 @@
 package rocks.friedrich.one_two_three_weight;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     TextView pointsTextView;
     TextView pointsPerPortionTextView;
 
+    ImageView indicatorImage;
+
     Formula formula = new Formula();
 
     public void updateTextViews() {
@@ -28,12 +33,42 @@ public class MainActivity extends AppCompatActivity {
             pointsTextView.setText(R.string.points);
         }
 
+
         double pointsPerPortion = formula.calculatePointsPerPortion(points);
         if (pointsPerPortion > 0) {
             pointsPerPortionTextView.setText(formula.roundToString(pointsPerPortion));
         } else {
             pointsPerPortionTextView.setText(R.string.points_per_portion);
         }
+
+        if (pointsPerPortion > 0) {
+            updateIndicatorImage(pointsPerPortion);
+        } else {
+            updateIndicatorImage(points);
+        }
+    }
+
+    public void updateIndicatorImage(double points) {
+        int image;
+        int color;
+
+        if (points <= 4) {
+            image = R.drawable.slim;
+            color = R.color.green_500;
+
+        } else if (points > 4 && points < 8) {
+            image = R.drawable.medium;
+            color = R.color.yellow_500;
+
+        } else {
+            image = R.drawable.fat;
+            color = R.color.red_500;
+        }
+
+        indicatorImage.setImageResource(image);
+
+        indicatorImage.setColorFilter(ContextCompat.getColor(MainActivity.this,
+                color));
     }
 
     public void resetForNewCalculation() {
@@ -57,6 +92,8 @@ public class MainActivity extends AppCompatActivity {
 
         pointsTextView = findViewById(R.id.points_text_view);
         pointsPerPortionTextView = findViewById(R.id.points_per_portion_text_view);
+
+        indicatorImage = findViewById(R.id.indicator_image);
 
         caloriesEditText = findViewById(R.id.calories_edit_text);
         focusFirstEditText();
